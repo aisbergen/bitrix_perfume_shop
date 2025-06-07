9f05a62ca446957c1a96bc7661e313ec242e7f76
Aisulu <aisulukakharovaa@gmail.com>
2025-06-07 18:44:05 +0100

    added subsections in main menu

--- templates/sillage/components/bitrix/catalog/bootstrap_v5/template.php
+++ templates/sillage/components/bitrix/catalog/bootstrap_v5/template.php
@@ -1,13 +1,11 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<php $APPLICATION->SetAdditionalCSS($templateFolder."/style.css") ?>
 
 
-<div class="catalog-sections">
-    <?php foreach ($arResult['SECTIONS'] as $arSection): ?>
-        <div class="catalog-section">
-            <h2 class="catalog-title"><?= $arSection["NAME"] ?></h2>
-            <?php if ($arSection["PICTURE"]["SRC"]): ?>
-                <img src="<?= $arSection["PICTURE"]["SRC"] ?>" alt="<?= $arSection["NAME"] ?>" />
-            <?php endif; ?>
-        </div>
-    <?php endforeach; ?>
-</div>
+    <div class="catalog-sections">
+        <?php foreach ($arResult['SECTIONS'] as &$arSection): ?>
+            <a class="catalog-section-link" href="<?= $arSection["SECTION_PAGE_URL"] ?>">
+                <?= $arSection["NAME"] ?> (<?= $arSection["ELEMENT_CNT"] ?>)
+            </a>
+        <?php endforeach; ?>
+    </div>
